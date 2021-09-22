c020f7b1da3e688bd5f6337d4bc8f89cf9dc721a
obaby <obaby.lh@gmail.com>
2021-09-22 22:28:14 +0800

    调整worker数量

--- train_ads.py
+++ train_ads.py
@@ -448,7 +448,7 @@ def parse_opt(known=False):
     parser.add_argument('--single-cls', default=True,action='store_true', help='train multi-class data as single-class')
     parser.add_argument('--adam', action='store_true', help='use torch.optim.Adam() optimizer')
     parser.add_argument('--sync-bn', action='store_true', help='use SyncBatchNorm, only available in DDP mode')
-    parser.add_argument('--workers', type=int, default=8, help='maximum number of dataloader workers')
+    parser.add_argument('--workers', type=int, default=2, help='maximum number of dataloader workers')
     parser.add_argument('--project', default='runs/train', help='save to project/name')
     parser.add_argument('--entity', default=None, help='W&B entity')
     parser.add_argument('--name', default='exp', help='save to project/name')
